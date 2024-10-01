3246f2ba9762cac7a47483f28c8f5652d4301794
Bharathi Shankar J <bharathishankarj@cookr.in>
2024-10-01 10:09:58 +0530

    10/01/2024 Leet Code Daily

---
+++ check_if_array_pairs_are_divisible_by_k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        freq=[0]*k
+        for  i in arr:
+            remainder=(i%k+k)%k
+            freq[remainder]+=1
+        
+        print(freq)
+        if freq[0]%2!=0:
+            return False
+        for i in range(1,k//2+1):
+            if freq[i]!=freq[k-i]:
+                return False
+
+        return True
+
+        
--- max_area_in_binary_matrix.py
+++ max_area_in_binary_matrix.py
@@ -23,5 +23,53 @@ for i in range(1,row):
 
 
 
-def MAH(arr):
-    
+def MAH(arr,lst):
+    ngr=[-1]*len(arr)
+
+    stack=[]
+    for i in range(len(arr)-1,-1,-1):
+
+        while len(stack)>0 and stack[-1][0]>=arr[i]:
+            stack.pop()
+        if len(stack)==0:
+            pass
+        if len(stack)>0:
+            if stack[-1][0]<arr[i]:
+                ngr[i]=stack[-1][1]
+            
+        stack.append((arr[i],i))
+
+
+    ngl=[-1]*len(arr)
+
+    stack=[]
+    for i in range(len(arr)):
+        while len(stack)>0 and stack[-1][0]>=arr[i]:
+            stack.pop()
+        if len(stack)==0:
+            pass
+        if len(stack)>0:
+            if stack[-1][0]<arr[i]:
+                ngl[i]=stack[-1][1]
+            
+        stack.append((arr[i],i))
+
+    finals=[]
+    for i in range(len(arr)):
+        if ngr[i]==-1 or ngl[i]==-1:
+            sums=1*arr[i]
+            finals.append(sums)
+        else:
+
+            sums=ngr[i]-ngl[i]-1
+            sums=sums*arr[i]
+            finals.append(sums)
+
+    lst.append(max(finals))
+
+
+
+lst=[] 
+for i in finals:
+    MAH(i,lst)
+print(lst)
